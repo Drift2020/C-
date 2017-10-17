76db1722bd72f293cf792a425f507eb2f4d60ab0
Drift2020 <linkor1888@gmail.com>
2017-10-17 12:55:24 +0300

    Pilot
    StartAircraft
    StartDispatcher

---
+++ Airplane pilot simulator/24-7/Aircraft.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_7
+{
+    delegate void Dis(object Aircraft);
+    class Aircraft
+    {
+        int Height { get; set; }
+        int Speed { get; set; }
+        List<Dis> list = new List<Dis>();
+        
+
+        public event Dis observation
+        {
+            // Используем аксессоры событий
+            add
+            {
+                list.Add(value);
+            }
+
+            remove
+            {
+                list.Remove(value);
+            }
+        }
+        public void Generator_Event_Observation()
+        {
+            Console.WriteLine("Произошло событие!!!");
+            if (list.Count != 0)
+                foreach (Dis i in list)
+                {
+                    i(this);
+                }
+        }
+    }
+}
---
+++ Airplane pilot simulator/24-7/Start.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_7
+{
+    class Start
+    {
+
+        void Print_Start_Menu()
+        {
+            Console.WriteLine("1. Start simulator\nEsc. Exit");
+        }
+        public void Main()
+        {
+            do
+            {
+
+            } while (true);
+        }
+    }
+}
